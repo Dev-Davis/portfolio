2d8f6a8abaffe50833fd245dfbec051ebd12234a
Sean Davis <sean.dev.davis@gmail.com>
2020-08-01 17:36:47 -0500

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Navbar from './Components/Navbar/Navbar'
+import About from './Components/About/About'
+import Techs from './Components/Technologies/Technologies'
+import Projects from './Components/Projects/Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <About />
+      <Techs />
+      <Projects />
     </div>
   );
 }
