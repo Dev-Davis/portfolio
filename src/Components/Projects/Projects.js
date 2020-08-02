9e61dbcd58d54a1db5b372afba0b8c4d0c6f5e8c
Sean Davis <sean.dev.davis@gmail.com>
2020-08-02 07:27:23 -0500

    Added projects to the site

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,43 @@
 import React from 'react'
 
-export default function Projects() {
-    return (
-        <h1>Projects</h1>
-    )
+import projectData from '../../helpers/data/projectsData'
+import ProjectCard from '../ProjectCards/ProjectsCard';
+import './Projects.css'
+
+class Projects extends React.Component {
+    state = {
+        projects: [],
+    }
+
+    getProjects = () => {
+        projectData.getProjects()
+            .then(projects => this.setState({ projects }))
+            .catch(err => console.error('could not get projects', err));
+    }
+
+    componentDidMount = () => {
+        this.getProjects();
+    }
+
+    render() {
+        const cards = this.state.projects.map(project => (
+            <ProjectCard
+                key={project.id}
+                project={project}
+            />
+        ))
+        return (
+            <div className="Projects col-12">
+                <h1>Projects</h1>
+                <div>
+                    <div className="col-3">
+                        {cards}
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
 }
+
+export default Projects
